b25955ba38e58422c64ba986330a5cce5d8aaac6
alinizam <alinizam99@gmail.com>
2018-04-17 13:24:28 +0300

    Giriş Dersi

---
+++ BLM104_2017_2018Bahar/src/Ders10/Unvan.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ders10;
+
+/**
+ *
+ * @author anizam
+ */
+public class Unvan {
+    int id;
+    String adi;
+
+    @Override
+    public String toString() {
+        return adi; //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
